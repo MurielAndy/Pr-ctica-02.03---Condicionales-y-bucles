1200453908d1d9f80a0ced52e15c5ccc18fb4482
xgarciaech1 <xgarciaech@educacion.navarra.es>
2024-10-21 11:16:28 +0200

    Create Ejercicio01.py

---
+++ Ejercicio01.py
@@ -0,0 +1,8 @@
+#Escribir un programa que almacene la cadena de caracteres contraseña en una
+#variable, pregunte al usuario por la contraseña e imprima por pantalla si la
+#contraseña introducida por el usuario coincide con la guardada en la variable
+#sin tener en cuenta mayúsculas y minúsculas
+palabra = "python"
+
+for i in range(len(palabra)):
+    print(i)
