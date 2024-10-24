e8211503e33f888f6e8db71f7bd017581f461ab1
xgarciaech1 <xgarciaech@educacion.navarra.es>
2024-10-24 20:15:24 +0200

    Update Ejercicio06.py

---
+++ Ejercicio06.py
@@ -0,0 +1,5 @@
+#Escribir un programa que pregunte al usuario su edad y muestre por pantalla 
+#todos los años que ha cumplido (desde 1 hasta su edad).
+años = input("Cuantos años tienes: ")
+for i in range(1, años + 1):
+    print(i)
