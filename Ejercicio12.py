28b75877ddaf31be99e9e94a012db3800ca3be7d
xgarciaech1 <xgarciaech@educacion.navarra.es>
2024-10-25 12:39:40 +0200

    Update Ejercicio12.py

--- Ejercicio12.py
+++ Ejercicio12.py
@@ -3,5 +3,9 @@
 #frase.
 frase = input("Escribeme una frase: ")
 letra = input("Dime de la frase una letra: ")
-for letra in range(len(frase), 1, 1):
-    print(letra)
+suma = 0
+for i in frase:
+    if i == letra:
+        suma += 1
+print("la letra", letra, "aparecece", suma, "veces")
+    
